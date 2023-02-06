18ef0b03099081204e5e6e5a2a3a7efcd07098f2
akouvivien <akouvivien@gmail.com>
2023-02-06 16:41:45 +0000

    add

--- Exercice_3/src/App.java
+++ Exercice_3/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Vehicle vehicle = new Vehicle( "tesla", "100");
         Car  car = new Car("Roadster");
         car.setSpeed("200");
         car.setModel("Tesla");
